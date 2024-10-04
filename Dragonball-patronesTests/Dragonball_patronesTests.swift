9eb6fb327d57340d53c4861a476daf01830a7b29
Sergio Gracia Jimenez <xshokunx@gmail.com>
2024-10-04 19:31:32 +0200

    Add base files

---
+++ Dragonball-patronesTests/Dragonball_patronesTests.swift
@@ -0,0 +1,17 @@
+//
+//  Dragonball_patronesTests.swift
+//  Dragonball-patronesTests
+//
+//  Created by Sergio Gracia Jimenez on 4/10/24.
+//
+
+import Testing
+@testable import Dragonball_patrones
+
+struct Dragonball_patronesTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
