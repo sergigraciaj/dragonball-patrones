f269d994e21795ec7128e6d1ae98b6c8b7f12ab1
Sergio Gracia Jimenez <xshokunx@gmail.com>
2024-10-05 15:42:01 +0200

    Add tests

---
+++ Dragonball-patronesTests/UseCaseTests/LoginUseCaseTests.swift
@@ -0,0 +1,73 @@
+//
+//  LoginUseCaseTests.swift
+//  Dragonball-patrones
+//
+//  Created by Sergio Gracia Jimenez on 5/10/24.
+//
+
+@testable import Dragonball_patrones
+import XCTest
+
+final class APISessionMock: APISessionContract {
+    let mockResponse: ((any APIRequest) -> Result<Data, any Error>)
+    
+    init(mockResponse: @escaping (any APIRequest) -> Result<Data, any Error>) {
+        self.mockResponse = mockResponse
+    }
+    
+    func request<Request: APIRequest>(apiRequest: Request, completion: @escaping (Result<Data, any Error>) -> Void) {
+        completion(mockResponse(apiRequest))
+    }
+}
+
+final class DummySessionDataSource: SessionDataSourceContract {
+    private(set) var session: Data?
+        
+    func storeSession(_ session: Data) {
+        self.session = session
+    }
+    
+    func getSession() -> Data? { nil }
+
+}
+
+final class LoginUseCaseTests: XCTestCase {
+    func testSuccessStoresToken() {
+        let dataSource = DummySessionDataSource()
+        let sut = LoginUseCase(dataSource: dataSource)
+        
+        let expectation = self.expectation(description: "TestSuccess")
+        let data = Data("hello-world".utf8)
+        
+        APISession.shared = APISessionMock { _ in .success(data) }
+        sut.execute(credentials: Credentials(username: "a@b.es", password: "122345")) { result in
+            guard case .success = result else {
+                return
+            }
+            expectation.fulfill()
+        }
+        
+        waitForExpectations(timeout: 5)
+        XCTAssertEqual(dataSource.session, data)
+    }
+    
+    func testFailureDoesNotStoreToken() {
+        let dataSource = DummySessionDataSource()
+        let sut = LoginUseCase(dataSource: dataSource)
+        
+        let expectation = self.expectation(description: "TestFailure")
+        APISession.shared = APISessionMock { _ in .failure(APIErrorResponse.network("login-fail")) }
+        
+        sut.execute(credentials: Credentials(username: "a@b.es", password: "122345")) { result in
+            guard case .failure = result else {
+                return
+            }
+            expectation.fulfill()
+        }
+        
+        waitForExpectations(timeout: 5)
+        XCTAssertNil(dataSource.session)
+    }
+    
+    
+}
